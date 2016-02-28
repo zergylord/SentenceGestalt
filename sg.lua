9ed24f63d02f5bc7b91a33c14feacc2d3a5d87cf
Steven Hansen <zergylord@gmail.com>
2016-02-27 21:17:35 -0800

    bug fix

--- sg.lua
+++ sg.lua
@@ -1,6 +1,7 @@
 require 'nngraph'
 require 'optim'
 util = require 'util/model_utils.lua'
+refresh = 1e3
 num_words_per_sentence = 7
 num_probes = 10
 
@@ -17,7 +18,7 @@ probe_dim = 85
 role_dim = 85
 local probe_input = nn.Identity()()
 local sg_input = nn.Identity()()
-local hid = nn.ReLU()(nn.Linear(probe_dim+sg_dim,hid_dim)(nn.JoinTable(1){probe_input,sg_input}))
+local hid = nn.ReLU()(nn.Linear(probe_dim+sg_dim,role_dim)(nn.JoinTable(1){probe_input,sg_input}))
 local role = nn.Sigmoid()(hid)
 probe_net = nn.gModule({probe_input,sg_input},{role})
 
@@ -38,7 +39,7 @@ out = sg_net:forward{torch.rand(word_dim),out}
 bce_crit = nn.BCECriterion()
 
 probe = torch.rand(num_probes,probe_dim)
-role_targets = torch.rand(num_probes,role_dim)
+role_targets = torch.rand(num_probes,role_dim):gt(.5):double()
 timer = torch.Timer()
 
 train = function(x)
